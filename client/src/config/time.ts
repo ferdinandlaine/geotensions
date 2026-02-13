af3eedb6a8e7ac44c0810b89bc3678793fc9e90a
ferdinandlaine <laineferdinand@gmail.com>
2026-02-13 23:25:54 +0100

    fix(client): update time config to use startOfDay for coverage end date

--- client/src/config/time.ts
+++ client/src/config/time.ts
@@ -1,6 +1,6 @@
-import { subYears } from 'date-fns'
+import { startOfDay, subYears } from 'date-fns'
 
 export const TIME_CONFIG = {
   COVERAGE_START_DATE: new Date(2020, 0, 1),
-  COVERAGE_END_DATE: subYears(new Date(), 1),
+  COVERAGE_END_DATE: subYears(startOfDay(new Date()), 1),
 } as const
