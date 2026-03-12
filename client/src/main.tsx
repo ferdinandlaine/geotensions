c672f53288efb41f55afa5c6bf16c69475aa6387
ferdinandlaine <laineferdinand@gmail.com>
2026-03-12 19:10:37 +0100

    feat(client): add NotFound page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import App from './App'
+import NotFound from './pages/NotFound'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,6 +28,7 @@ createRoot(root).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
