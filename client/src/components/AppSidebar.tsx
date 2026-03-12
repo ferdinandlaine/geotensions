9fd8dbbdab7abb979e5ac8812c577864f30f9c32
ferdinandlaine <laineferdinand@gmail.com>
2026-03-12 19:01:55 +0100

    feat(client): add links in sidebar footer

--- client/src/components/AppSidebar.tsx
+++ client/src/components/AppSidebar.tsx
@@ -1,11 +1,40 @@
+import { IconBrandGithub } from '@tabler/icons-react'
 import type { ComponentProps } from 'react'
 
-import { Sidebar, SidebarContent } from '@/components/ui/sidebar'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarSeparator } from '@/components/ui/sidebar'
+
+import { Button } from './ui/button'
 
 function AppSidebar({ children, ...props }: ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
       <SidebarContent className="py-2">{children}</SidebarContent>
+      <SidebarSeparator className="mx-0" />
+      <SidebarFooter className="flex items-center gap-0">
+        <a
+          href="https://github.com/ferdinandlaine/geotensions"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="link">
+            <IconBrandGithub />
+            View on GitHub
+          </Button>
+        </a>
+
+        <p className="text-muted-foreground text-center text-xs">
+          Sources:{' '}
+          <a
+            href="https://acleddata.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            ACLED
+          </a>{' '}
+          © 2025
+        </p>
+      </SidebarFooter>
     </Sidebar>
   )
 }
