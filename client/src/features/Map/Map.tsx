524460b86d56cdc7ad33253b9291e508b7b7a069
ferdinandlaine <laineferdinand@gmail.com>
2026-04-22 01:07:45 +0200

    fix(client): disable touch pitch/rotate and compact the map attribution

--- client/src/features/Map/Map.tsx
+++ client/src/features/Map/Map.tsx
@@ -22,6 +22,11 @@ function Map({ children }: PropsWithChildren) {
 
   const handleMapLoad = (event: MapEvent) => {
     event.target.keyboard.disableRotation()
+    event.target
+      .getContainer()
+      .querySelector('.maplibregl-ctrl-attrib-button')
+      ?.setAttribute('tabIndex', '-1')
+    event.target.touchZoomRotate.disableRotation()
   }
 
   return (
@@ -37,8 +42,9 @@ function Map({ children }: PropsWithChildren) {
       maxZoom={MAP_CONFIG.MAX_ZOOM}
       maxBounds={MAP_CONFIG.MAX_BOUNDS}
       dragRotate={false}
+      touchPitch={false}
       renderWorldCopies={false}
-      attributionControl={false}
+      attributionControl={{ compact: true }}
       interactiveLayerIds={['events-layer']}
       onLoad={handleMapLoad}
     >
